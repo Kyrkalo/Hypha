5c5b0f06ad87fb44ba3ef181955fcad3adac6838
Roman Kyrkalo <roman.kyrkalo@live.com>
2024-10-21 17:26:06 -0400

    update

---
+++ Hypha.Core/Operations/Backward.cs
@@ -0,0 +1,17 @@
+﻿using Hypha.Attributes;
+using Hypha.Enums;
+using Hypha.Interfaces;
+using Hypha.Models;
+
+namespace Hypha.Operations;
+
+[Operation("1.0", ExecutionTypes.Backward)]
+internal class Backward : IOperation<Model, double[]>
+{
+    public string Name => throw new NotImplementedException();
+
+    public double[] Execute(IFunction normalization, IFunction function, Model t, double[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
